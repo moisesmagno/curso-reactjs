1f7039e0e7ba4c80954169b10f959b3609b1418a
Moisés S. E. Aguilar <moisesmagno@gmail.com>
2019-01-06 23:52:30 -0200

    Terminado parcialmente o desafio do app-github do rocketseat - goReact.

--- rocketseat/app-react/src/components/CompareList/index.js
+++ rocketseat/app-react/src/components/CompareList/index.js
@@ -1,75 +1,53 @@
 import React from 'react';
 import { Container, Repository } from './styles';
+import PropTypes from 'prop-types';
 
-const CompareList = () => (
+const CompareList = ({ repositories, removeRepository }) => (
   <Container>
-    <Repository>
-        <header>
-          <img src="https://avatars3.githubusercontent.com/u/69631?v=4" alt="" />
-          <strong>React</strong>
-          <small>Facebook</small>
-        </header>
-
-        <ul>
-          <li>
-            95,019 <small>starts</small>
-          </li>
-          <li>
-            95,019 <small>forks</small>
-          </li>
-          <li>
-            95,019 <small>issues</small>
-          </li>
-          <li>
-            3 days ago <small>last commit</small>
-          </li>
-        </ul>
-    </Repository>
-    <Repository>
-        <header>
-          <img src="https://avatars3.githubusercontent.com/u/69631?v=4" alt="" />
-          <strong>React</strong>
-          <small>Facebook</small>
-        </header>
-
-        <ul>
-          <li>
-            95,019 <small>starts</small>
-          </li>
-          <li>
-            95,019 <small>forks</small>
-          </li>
-          <li>
-            95,019 <small>issues</small>
-          </li>
-          <li>
-            3 days ago <small>last commit</small>
-          </li>
-        </ul>
-    </Repository>
-    <Repository>
-        <header>
-          <img src="https://avatars3.githubusercontent.com/u/69631?v=4" alt="" />
-          <strong>React</strong>
-          <small>Facebook</small>
-        </header>
-
-        <ul>
-          <li>
-            95,019 <small>starts</small>
-          </li>
-          <li>
-            95,019 <small>forks</small>
-          </li>
-          <li>
-            95,019 <small>issues</small>
-          </li>
-          <li>
-            3 days ago <small>last commit</small>
-          </li>
-        </ul>
-    </Repository>
+    {
+      repositories.map(repository => (
+        <Repository key={repository.id}>
+          <header>
+            <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+            <strong>{repository.name}</strong>
+            <small>{repository.owner.login}</small>
+          </header>
+          <ul>
+            <li>
+              {repository.stargazers_count} <small>starts</small>
+            </li>
+            <li>
+              {repository.forks_count} <small>forks</small>
+            </li>
+            <li>
+              {repository.open_issues_count} <small>issues</small>
+            </li>
+            <li>
+              {repository.lastCommit} <small>last commit</small>
+            </li>
+            <li>
+              <button onClick={() => removeRepository(repository.id)}>Remover</button>
+            </li>
+          </ul>
+        </Repository>
+      ))
+    }
   </Container>
 );
+
+CompareList.propTypes = {
+  repositories: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number,
+    name: PropTypes.string,
+    owner: PropTypes.shape({
+      login: PropTypes.string,
+      avatar_url: PropTypes.string,
+    }),
+    stargazers_count: PropTypes.number,
+    forks_count: PropTypes.number,
+    open_issues_count: PropTypes.number,
+    pushed_at: PropTypes.string,
+  })).isRequired,
+}
 
 export default CompareList;
--- rocketseat/app-react/src/pages/Main/index.js
+++ rocketseat/app-react/src/pages/Main/index.js
@@ -1,19 +1,90 @@
-import React from 'react';
+import React, { Component } from 'react';
+import api from '../../services/api';
 import { Container, Form } from './styles';
 import logo from '../../assets/logo.png';
 import CompareList from '../../components/CompareList/index';
+import moment from 'moment';
 
-const Main = () => (
-  <Container>
-    <img src={logo} alt="Github Compare" />
-    <Form>
-      <input type="text" placeholder="usuário/repositorio" />
-      <button type="submit">Buscar</button>
-    </Form>
+class Main extends Component {
 
-    <CompareList />
+  state = {
+    repositoryError: false,
+    repositoryInput: '',
+    repositories: [],
+    loading: false,
+  }
 
-  </Container>
-);
+  handleAddRepository = async (event) => {
+    event.preventDefault();
+
+    this.setState({ loading: true });
+
+    try{
+
+      const { data: repository } = await api.get(`/repos/${this.state.repositoryInput}`);
+
+      repository.lastCommit = moment(repository.pushed_at).fromNow();
+
+      this.setState({
+        repositoryInput: '',
+        repositories: [...this.state.repositories, repository],
+        repositoryError: false,
+      });
+
+      // Recording the repositories in Local Storage.
+      localStorage.setItem('repository', JSON.stringify(this.state.repositories));
+      console.log(JSON.parse(localStorage.getItem('repository')));
+
+    } catch (err) {
+
+      this.setState({
+        repositoryError: true,
+      });
+
+    } finally {
+      this.setState({ loading: false });
+    }
+  }
+
+  captureValueInput = (event) => {
+    this.setState({
+      repositoryInput: event.target.value,
+    });
+  }
+
+  removeRepositoryClick = (id) => {
+    this.setState({
+      repositories: this.state.repositories.filter(repository => repository.id !== id),
+    });
+
+    const currentLocalStorage = JSON.parse(localStorage.getItem('repository'));
+    const updatedLocalStorage = currentLocalStorage.filter(repository => repository.id !== id);
+    localStorage.setItem('repository', JSON.stringify(updatedLocalStorage));
+
+    console.log(JSON.parse(localStorage.getItem('repository')));
+  }
+
+  render () {
+    return (
+      <Container>
+
+        <img src={logo} alt="Github Compare" />
+
+        <Form withError={this.state.repositoryError} onSubmit={this.handleAddRepository}>
+          <input
+            type="text"
+            placeholder="usuário/repositorio"
+            value={this.state.repositoryInput}
+            onChange={ this.captureValueInput }
+          />
+          <button type="submit">{ this.state.loading ? <i className="fa fa-spinner fa-pulse" /> : 'OK' }</button>
+        </Form>
+
+        <CompareList repositories={this.state.repositories} removeRepository={this.removeRepositoryClick} />
+
+      </Container>
+    )
+  }
+}
 
 export default Main;
