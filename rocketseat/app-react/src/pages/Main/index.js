7707f7b812b5588efe4148e90ffc1ed139dd404a
Moisés S. E. Aguilar <moisesmagno@gmail.com>
2019-01-09 23:07:09 -0200

    Terminado o desafio do app-github

--- rocketseat/app-react/src/pages/Main/index.js
+++ rocketseat/app-react/src/pages/Main/index.js
@@ -6,84 +6,97 @@ import CompareList from '../../components/CompareList/index';
 import moment from 'moment';
 
 class Main extends Component {
-
   state = {
     repositoryError: false,
-    repositoryInput: '',
-    repositories: [],
-    loading: false,
-  }
+    repositoryInput: "",
+    repositories: JSON.parse(localStorage.getItem("repository")),
+    loading: false
+  };
 
-  handleAddRepository = async (event) => {
+  handleAddRepository = async event => {
     event.preventDefault();
 
     this.setState({ loading: true });
 
-    try{
-
-      const { data: repository } = await api.get(`/repos/${this.state.repositoryInput}`);
+    try {
+      const { data: repository } = await api.get(
+        `/repos/${this.state.repositoryInput}`
+      );
 
       repository.lastCommit = moment(repository.pushed_at).fromNow();
 
       this.setState({
-        repositoryInput: '',
+        repositoryInput: "",
         repositories: [...this.state.repositories, repository],
-        repositoryError: false,
+        repositoryError: false
       });
 
       // Recording the repositories in Local Storage.
-      localStorage.setItem('repository', JSON.stringify(this.state.repositories));
-      console.log(JSON.parse(localStorage.getItem('repository')));
+      localStorage.setItem(
+        "repository",
+        JSON.stringify(this.state.repositories)
+      );
 
+      console.log(localStorage.getItem("repository"));
     } catch (err) {
-
       this.setState({
-        repositoryError: true,
+        repositoryError: true
       });
-
     } finally {
       this.setState({ loading: false });
     }
-  }
+  };
 
-  captureValueInput = (event) => {
+  captureValueInput = event => {
     this.setState({
-      repositoryInput: event.target.value,
+      repositoryInput: event.target.value
     });
-  }
+  };
 
-  removeRepositoryClick = (id) => {
+  removeRepositoryClick = id => {
     this.setState({
-      repositories: this.state.repositories.filter(repository => repository.id !== id),
+      repositories: this.state.repositories.filter(
+        repository => repository.id !== id
+      )
     });
 
-    const currentLocalStorage = JSON.parse(localStorage.getItem('repository'));
-    const updatedLocalStorage = currentLocalStorage.filter(repository => repository.id !== id);
-    localStorage.setItem('repository', JSON.stringify(updatedLocalStorage));
+    const currentLocalStorage = JSON.parse(localStorage.getItem("repository"));
+    const updatedLocalStorage = currentLocalStorage.filter(
+      repository => repository.id !== id
+    );
+    localStorage.setItem("repository", JSON.stringify(updatedLocalStorage));
+  };
 
-    console.log(JSON.parse(localStorage.getItem('repository')));
-  }
-
-  render () {
+  render() {
     return (
       <Container>
-
         <img src={logo} alt="Github Compare" />
 
-        <Form withError={this.state.repositoryError} onSubmit={this.handleAddRepository}>
+        <Form
+          withError={this.state.repositoryError}
+          onSubmit={this.handleAddRepository}
+        >
           <input
             type="text"
             placeholder="usuário/repositorio"
             value={this.state.repositoryInput}
-            onChange={ this.captureValueInput }
+            onChange={this.captureValueInput}
           />
-          <button type="submit">{ this.state.loading ? <i className="fa fa-spinner fa-pulse" /> : 'OK' }</button>
+          <button type="submit">
+            {this.state.loading ? (
+              <i className="fa fa-spinner fa-pulse" />
+            ) : (
+              "OK"
+            )}
+          </button>
         </Form>
 
-        <CompareList repositories={this.state.repositories} removeRepository={this.removeRepositoryClick} />
-
+        <CompareList
+          repositories={this.state.repositories}
+          removeRepository={this.removeRepositoryClick}
+        />
       </Container>
-    )
+    );
   }
 }
 
